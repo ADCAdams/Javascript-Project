611652ff6ffa01d4c0e72d57a9028321f6eb2db4
adcadams <andrewdcadams@gmail.com>
2021-02-21 13:32:13 -0500

    posts matches. appends winr to dom

--- frontend/src/match.js
+++ frontend/src/match.js
@@ -1,16 +1,60 @@
 class Match {
 
-    constructor(radiantHeroArray,direHeroArary){
+    constructor(){
         this.matchHeros = [] //purpose of checking for a unique hero list later
         this.winner = ""
+        this.radiantTeamInstance = []
+        this.direTeamInstance = []
         this.winnerProbability = 0
-        this.matchHeros.push(radiantArray)
-        this.matchHeros.push(direArray)
-        this.calculateMatchWinner(radiantHeroArray,direHeroArary)
     }
 
+
+
+    makeTeams() {
+        this.matchHeros = []
+        Match.clearHeroList("radiantListID");
+        Match.clearHeroList("direListID");
+        Match.clearWinners;
+    
+        
+ 
+        apiService.getHeros().then(herosResp => { 
+            let allHeros = []
+            let rawHerosForMatch = []
+            allHeros = herosResp.data //makes array of heros
+    
+    
+            while (this.matchHeros.length < 10){ //gets 10- heros
+                const rawHero = allHeros[Math.floor(Math.random() * allHeros.length)]
+                if (!rawHerosForMatch.includes(rawHero)){
+                    rawHerosForMatch.push(rawHero)
+                    const newHero = new Hero(rawHero)
+                    this.matchHeros.push(newHero) //adds unique heros to the array
+                }
+            }
+            this.radiantTeamInstance = this.matchHeros.slice(0,5); //splits on to two teams
+            this.direTeamInstance = this.matchHeros.slice(5,10);
+            
+    
+            this.radiantTeamInstance.forEach(hero => {
+               Match.appendRadiantHero(hero);
+            })
+            this.direTeamInstance.forEach(hero => {
+                Match.appendDireHero(hero);
+             })
+
+             this.calculateMatchWinner();
+
+        }) //ends get promise
+
+
+         
+    }
+
+
+    
      calculateTeamWinAvg(teamHeroArray){
-        const teamWinAvg = 0
+        let teamWinAvg = 0
         teamHeroArray.forEach(function (hero){
             teamWinAvg += hero.winrate
         })
@@ -18,13 +62,14 @@ class Match {
         return teamWinAvg
     }
     
-     calculateMatchWinner(radiantHeroArray,direHeroArary){
-         const radiantWinAvg = calculateTeamWinAvg(radiantHeroArray)
-         const direWinAvg = calculateTeamWinAvg(direHeroArary)
+     calculateMatchWinner(){
+         const radiantWinAvg = this.calculateTeamWinAvg(this.radiantTeamInstance)
+         const direWinAvg = this.calculateTeamWinAvg(this.direTeamInstance)
         if (radiantWinAvg > direWinAvg){
             this.winner = "Radiant"
             const delta = radiantWinAvg - direWinAvg
             this.winnerProbability = (delta/.20)  //or 'liklihood'
+            
         } else if (direWinAvg > radiantWinAvg){
             this.winner = "Dire"
             const delta =  direWinAvg - radiantWinAvg
@@ -32,10 +77,66 @@ class Match {
         } else {
             this.winner = "Tie"
         }
+        this.appendWinner(this.winner,this.winnerProbability)
     }
  
 
+    appendWinner(winnerArg,winProbArg){
+        let winSpan = document.createElement('span')
+        winSpan.className = "winnerSpan"
+        winSpan.innerHTML = 
+        `Winner: ${winnerArg}`;
+
+        let winProbSpan = document.createElement('span');
+        winProbSpan.className = "winnerSpan"
+        winProbSpan.innerHTML = 
+        `Probability of victory: ${(winProbArg*100).toFixed(2)+"%"}`;
+
+        let winContainer = document.getElementById("containerWinner")
+        winContainer.append(winSpan);
+        winContainer.append(winProbSpan);
+    }
+
+
     
+
+     static renderHero(hero){
+        let newli = document.createElement('li')
+        newli.className="heroListItem"
+        newli.innerHTML = `<span class="heroLiSpan"> <span class="heroNameSpan">
+        ${hero.name}
+        </span>
+        <span class="heroWinrateSpan">
+        ${parseFloat(hero.winrate*100).toFixed(2)+"%"}
+        </span> </span>`
+        return newli
+    }
+    
+     static appendRadiantHero(hero) {
+        let radiantUL = document.getElementById("radiantListID")
+    
+        let liHero = Match.renderHero(hero)
+        radiantUL.append(liHero)
+    
+    }
+     static appendDireHero(hero) {
+        let direUL = document.getElementById("direListID")
+    
+        let liHero = Match.renderHero(hero)
+        direUL.append(liHero)
+    
+    }
+    
+
+    static clearHeroList(listID){
+        var ul = document.getElementById(listID);
+        while(ul.firstChild) ul.removeChild(ul.firstChild);
+    }
+
+    static clearWinners(){
+        let containDiv = document.getElementById("containerWinner")
+        while(containDiv.firstChild) containDiv.removeChild(containDiv.firstChild);
+    }
 
 
 
